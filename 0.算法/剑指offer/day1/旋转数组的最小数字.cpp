6826082c271f66c5a4b77bfc884d2c6fb7ee8b7e
PangHahMan <3278475092@qq.com>
2023-05-03 23:15:21 +0800

    1

--- 0.算法/剑指offer/day1/旋转数组的最小数字.cpp
+++ 0.算法/剑指offer/day1/旋转数组的最小数字.cpp
@@ -116,22 +116,3 @@ public:
     }
 };
 
-class Solution
-{
-public:
-    int minNumberInRotateArray(vector<int> rotateArray)
-    {
-        int left = 0, right = rotateArray.size() - 1;
-        while (left < right)
-        {
-            int mid = left + (right - left) / 2; // 取中间值
-            if (rotateArray[mid] > rotateArray[right])
-                left = mid + 1; // 搜索右边
-            else if (rotateArray[mid] < rotateArray[right])
-                right = mid; // 搜索左边
-            else
-                right -= 1;  
-        }
-        return rotateArray[left];
-    }
-};
