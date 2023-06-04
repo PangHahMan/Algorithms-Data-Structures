6371e6f24dc82211cf52133972ee18b6a29a12b8
PangHahMan <3278475092@qq.com>
2023-06-04 23:01:09 +0800

    位图

--- 6.二叉树进阶/哈希表/位图/bitset.h
+++ 6.二叉树进阶/哈希表/位图/bitset.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 #include <string>
 #include <time.h>
 #include <vector>
@@ -30,9 +31,50 @@ public:
         size_t i = x / 8;
         size_t j = x % 8;
 
-        return _bits[i] % (1 << j);//测试比特位是否为1
+        return _bits[i] & (1 << j);//测试比特位是否为1
     }
 
 private:
     vector<char> _bits;//位图底层结构
+};
+
+
+//给定100亿个整数，设计只出现一次的整数？
+//思路：设计两个位图，第一个位图出现为1，再次出现就将第二个位图设为1，最后判断第二个位图为0就是出现一次
+template<size_t N>
+class twobitset {
+public:
+    void set(size_t x) {
+        //新插入的值放在bs2中
+        if (_bs1.test(x) == false && _bs2.test(x) == false) {
+            _bs2.set(x);
+        }
+        //如果在插入，就将bs2reset，插入到bs1中，那么只要判断ps2是否为真即是出现一次的
+        else if (_bs1.test(x) == false && _bs2.test(x) == true) {
+            _bs1.set(x);
+            _bs2.reset(x);
+        }
+    }
+
+    void Print() {
+        for (size_t i = 0; i < N; ++i) {
+            if (_bs2.test(i)) {
+                cout << i << endl;
+            }
+        }
+    }
+
+private:
+    bitset<N> _bs1;
+    bitset<N> _bs2;
+};
+
+
+#pragma once
+
+template<size_t N>
+class BloomFilter {
+public:
+
+private:
 };
--- 6.二叉树进阶/哈希表/位图/test.cpp
+++ 6.二叉树进阶/哈希表/位图/test.cpp
@@ -1,8 +1,48 @@
+#include "bitset.h"
 #include <iostream>
-
 using namespace std;
+void test_bitset1() {
+    bitset<100> bs;
+    bs.set(10);
+    bs.set(11);
+    bs.set(15);
+
+    cout << bs.test(10) << endl;//1
+    cout << bs.test(15) << endl;//1
+
+    bs.reset(10);
+
+    cout << bs.test(10) << endl;//0
+    cout << bs.test(15) << endl;//1
+
+    bs.reset(10);
+    bs.reset(15);
+
+    cout << bs.test(10) << endl;//0
+    cout << bs.test(10) << endl;//0
+}
+
+//如何开数据量很大的空间
+void test_bitset2() {
+    
+    bitset<-1> bs;   //会开512MB的空间，整型的最大值
+    //bitset<0xFFFFFFFF> bs;
+
+}
+
+void test_bitset3(){
+    int a[] = { 3, 45, 53, 32, 32, 43, 3, 2, 5, 2, 32, 55, 5, 53,43,9,8,7,8 };
+	twobitset<100> bs;
+	for (auto e : a)
+	{
+		bs.set(e);    //7 9 45 55
+	}
+
+    bs.Print();
+	
+}
 
 int main() {
-
+    test_bitset3();
     return 0;
 }
