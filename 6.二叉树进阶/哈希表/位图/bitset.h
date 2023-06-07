b1ce8c8cee0b0e65c3ef0c3a28e7ad839ff662a8
PangHahMan <3278475092@qq.com>
2023-06-07 12:26:22 +0800

    1

--- 6.二叉树进阶/哈希表/位图/bitset.h
+++ 6.二叉树进阶/哈希表/位图/bitset.h
@@ -69,12 +69,92 @@ private:
     bitset<N> _bs2;
 };
 
+//三种映射方式，映射到三个位置
+struct BKDRHash {
+    size_t operator()(const string &s) {
+        size_t hash = 0;
+        for (auto ch: s) {
+            hash += ch;
+            hash *= 31;
+        }
 
-#pragma once
+        return hash;
+    }
+};
 
-template<size_t N>
+struct APHash {
+    size_t operator()(const string &s) {
+        size_t hash = 0;
+        for (long i = 0; i < s.size(); i++) {
+            size_t ch = s[i];
+            if ((i & 1) == 0) {
+                hash ^= ((hash << 7) ^ ch ^ (hash >> 3));
+            } else {
+                hash ^= (~((hash << 11) ^ ch ^ (hash >> 5)));
+            }
+        }
+        return hash;
+    }
+};
+
+
+struct DJBHash {
+    size_t operator()(const string &s) {
+        size_t hash = 5381;
+        for (auto ch: s) {
+            hash += (hash << 5) + ch;
+        }
+        return hash;
+    }
+};
+
+template<size_t N,
+         class K = string,
+         class Hash1 = BKDRHash,
+         class Hash2 = APHash,
+         class Hash3 = DJBHash>
 class BloomFilter {
 public:
+    void set(const K &key) {
+        size_t len = N * _x;
+        size_t hash1 = Hash1()(key) % len;
+        _bs.set(hash1);
+
+        size_t hash2 = Hash2()(key) % len;
+        _bs.set(hash2);
+
+        size_t hash3 = Hash3()(key) % len;
+        _bs.set(hash3);
+    }
+
+
+    bool test(const K &key) {
+        size_t len = N * _x;
+
+
+        //测试三个如果都在，才返回true
+        size_t hash1 = Hash1()(key) % len;
+        if (!_bs.test(hash1)) {
+            return false;
+        }
+
+        size_t hash2 = Hash2()(key) % len;
+        if (!_bs.test(hash2)) {
+            return false;
+        }
+
+        size_t hash3 = Hash3()(key) % len;
+        if (!_bs.test(hash3)) {
+            return false;
+        }
+
+        // 在      不准确的，存在误判  - 可能在的位置是别数据的映射的
+        // 不在    准确的
+
+        return true;
+    }
 
 private:
+    static const size_t _x = 6;
+    bitset<N * _x> _bs;
 };
--- 6.二叉树进阶/哈希表/位图/test.cpp
+++ 6.二叉树进阶/哈希表/位图/test.cpp
@@ -1,4 +1,6 @@
 #include "bitset.h"
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 using namespace std;
 void test_bitset1() {
@@ -24,25 +26,93 @@ void test_bitset1() {
 
 //如何开数据量很大的空间
 void test_bitset2() {
-    
-    bitset<-1> bs;   //会开512MB的空间，整型的最大值
-    //bitset<0xFFFFFFFF> bs;
 
+    bitset<-1> bs;//会开512MB的空间，整型的最大值
+                  //bitset<0xFFFFFFFF> bs;
 }
 
-void test_bitset3(){
-    int a[] = { 3, 45, 53, 32, 32, 43, 3, 2, 5, 2, 32, 55, 5, 53,43,9,8,7,8 };
-	twobitset<100> bs;
-	for (auto e : a)
-	{
-		bs.set(e);    //7 9 45 55
-	}
+void test_bitset3() {
+    int a[] = {3, 45, 53, 32, 32, 43, 3, 2, 5, 2, 32, 55, 5, 53, 43, 9, 8, 7, 8};
+    twobitset<100> bs;
+    for (auto e: a) {
+        bs.set(e);//7 9 45 55
+    }
 
     bs.Print();
-	
+}
+
+void test_bloomfilter1() {
+    BloomFilter<100> bs;
+    bs.set("sort");
+    bs.set("bloom");
+    bs.set("hello world hello bit");
+    bs.set("test");
+    bs.set("etst");
+    bs.set("estt");
+
+    cout << bs.test("sort") << endl;                 //1
+    cout << bs.test("bloom") << endl;                //1
+    cout << bs.test("hello world hello bit") << endl;//1
+    cout << bs.test("etst") << endl;                 //1
+    cout << bs.test("test") << endl;                 //1
+    cout << bs.test("estt") << endl;                 //1
+
+    cout << bs.test("ssort") << endl;//0
+    cout << bs.test("tors") << endl; //0
+    cout << bs.test("ttes") << endl; //0
+}
+
+void test_bloomfilter2() {
+    srand(time(0));
+    const size_t N = 10000;
+    BloomFilter<N> bf;
+
+    std::vector<std::string> v1;
+    std::string url = "https://www.cnblogs.com/-clq/archive/2012/05/31/2528153.html";
+
+    for (size_t i = 0; i < N; ++i) {
+        v1.push_back(url + std::to_string(i));
+    }
+
+    for (auto &str: v1) {
+        bf.set(str);
+    }
+
+    // v2跟v1是相似字符串集，但是不一样
+    std::vector<std::string> v2;
+    for (size_t i = 0; i < N; ++i) {
+        std::string url = "https://www.cnblogs.com/-clq/archive/2012/05/31/2528153.html";
+        url += std::to_string(999999 + i);
+        v2.push_back(url);
+    }
+
+    size_t n2 = 0;
+    for (auto &str: v2) {
+        if (bf.test(str)) {
+            ++n2;
+        }
+    }
+    cout << "相似字符误判率：" << (double) n2 / (double) N << endl;
+
+    // 不相似字符串集
+    std::vector<std::string> v3;
+    for (size_t i = 0; i < N; ++i) {
+        string url = "zhihu.com";
+        //string url = "https://www.cctalk.com/m/statistics/live/16845432622875";
+        url += std::to_string(i + rand());
+        v3.push_back(url);
+    }
+
+    size_t n3 = 0;
+    for (auto &str: v3) {
+        if (bf.test(str)) {
+            ++n3;
+        }
+    }
+    cout << "不相似字符误判率：" << (double) n3 / (double) N << endl;
 }
 
 int main() {
-    test_bitset3();
+    test_bloomfilter2();
     return 0;
 }
