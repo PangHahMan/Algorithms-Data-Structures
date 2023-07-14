7561eec4e2897afe74a1cfd51a2b8d1b182bd799
PangHahMan <3278475092@qq.com>
2023-07-14 15:35:25 +0800

    简单

---
+++ 0.算法/剑指offer/day4/树的子结构.cpp
@@ -0,0 +1,59 @@
+/* 描述
+输入两棵二叉树A，B，判断B是不是A的子结构(不是子树)。 
+
+示例1
+输入：{8,8,7,9,2,#,#,#,#,4,7},{8,9,2}
+返回值：true
+
+示例2
+输入：{1,2,3,4,5},{2,4}
+返回值：true
+
+示例3
+输入：{1,2,3},{3,1}
+返回值：false
+*/
+
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+    }
+};
+
+
+class Solution {
+public:
+    bool isSubTree(TreeNode *root, TreeNode *subRoot) {
+        //递归条件
+        //两个都为nullptr,则说明相等,返回true
+        if (subRoot == nullptr) {
+            return true;
+        }
+
+        if (root == nullptr) {
+            return false;
+        }
+
+        if (root->val != subRoot->val) {
+            return false;
+        }
+        //左右树都想到才为true
+        return isSubTree(root->left, subRoot->left) && isSubTree(root->right, subRoot->right);
+    }
+
+    bool HasSubtree(TreeNode *pRoot1, TreeNode *pRoot2) {
+        if (pRoot1 == nullptr || pRoot2 == nullptr) {
+            return false;
+        }
+
+        //如果相等返回true
+        if (isSubTree(pRoot1, pRoot2))
+            return true;
+
+        //递归左右子树
+        return HasSubtree(pRoot1->left, pRoot2) || HasSubtree(pRoot1->right, pRoot2);
+    }
+};
