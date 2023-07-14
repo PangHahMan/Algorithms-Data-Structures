654ece1f64f1b6b50260668082968e5fdfe62ee1
PangHahMan <3278475092@qq.com>
2023-07-14 17:02:54 +0800

    简单

---
+++ 0.算法/算法训练/day5.连续最大和.cpp
@@ -0,0 +1,40 @@
+/* 描述
+一个数组有 N 个元素，求连续子数组的最大和。 例如：[-1,2,1]，和最大的连续子数组为[2,1]，其和为 3 
+
+示例1
+输入：3
+-1 2 1
+输出：3
+*/
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int getMax(int a, int b) {
+    return a > b ? a : b;
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    //输入n个值
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    int sum = v[0];
+    int max = v[0];
+    for (int i = 1; i < v.size(); i++) {
+        //v[i]和前面的值进行对比
+        sum = getMax(sum + v[i], v[i]);
+        if (sum > max) {
+            max = sum;
+        }
+    }
+    cout << max << endl;
+    return 0;
+}
