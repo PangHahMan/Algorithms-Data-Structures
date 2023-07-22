d6d86eec603e0fadb2a421cfad14279153cfe92f
PangHahMan <3278475092@qq.com>
2023-07-22 23:18:39 +0800

    简单

---
+++ 0.算法/算法训练/算法2/day11.最近公共祖先.cpp
@@ -0,0 +1,21 @@
+/* 描述
+将一棵无穷大满二叉树的结点按根结点一层一层地从左往右编号，根结点编号为1。现给定a，b为两个结点。设计一个算法，返回a、b最近的公共祖先的编号。注意其祖先也可能是结点本身。 
+测试样例：2，3
+返回：1
+*/
+
+#include <iostream>
+using namespace std;
+//思路：parent = child/2
+class LCA {
+public:
+    int getLCA(int a, int b) {
+        if (a == b) {
+            return a;
+        }
+
+        //9和3测试应该为1
+        return a > b ? getLCA(a / 2, b) : getLCA(a, b / 2);
+    }
+};
+
