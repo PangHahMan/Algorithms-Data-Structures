3d5aa98428e1000f59c7d1446462b110decfea7c
PangHahMan <3278475092@qq.com>
2023-08-10 00:00:08 +0800

    1

--- 1.顺序表/LeetCode/C-Code/189.轮转数组.c
+++ 1.顺序表/LeetCode/C-Code/189.轮转数组.c
@@ -1,40 +1,19 @@
-// LeetCode 189.��ת����
-// ����һ�����飬�������е�Ԫ��������ת k ��λ�ã����� k �ǷǸ�����
-/*
-ʾ�� 1:
-����: nums = [1,2,3,4,5,6,7], k = 3
-���: [5,6,7,1,2,3,4]
-����:
-������ת 1 ��: [7,1,2,3,4,5,6]
-������ת 2 ��: [6,7,1,2,3,4,5]
-������ת 3 ��: [5,6,7,1,2,3,4]
-*/
+
 #include <stdlib.h>
 
-
-/* ����һ��ʹ�ö��������
-void rotate(int *nums, int numsSize, int k)
-{
-    // ����������
-    int *newArr = (int *)malloc(sizeof(int) * numsSize);
-    // ��ԭ�����ֵ��ֵ���������k��λ�ã�(i+k)%numsSize�������γɻ���
-    for (int i = 0; i < numsSize; ++i)
-    {
+/* void rotate(int *nums, int numsSize, int k) {
+    int *newArr = (int *) malloc(sizeof(int) * numsSize);
+    for (int i = 0; i < numsSize; ++i) {
         newArr[(i + k) % numsSize] = nums[i];
     }
-    // ��󽫿��ٵ����������¿�������
-    for (int i = 0; i < numsSize; ++i)
-    {
+    for (int i = 0; i < numsSize; ++i) {
         nums[i] = newArr[i];
     }
-} 
-*/
+} */
 
-void reverse(int *a, int left, int right)
-{
-    // ���ý���
-    while (left < right)
-    {
+
+void reverse(int *a, int left, int right) {
+    while (left < right) {
         int tmp = a[left];
         a[left] = a[right];
         a[right] = tmp;
@@ -43,16 +22,10 @@ void reverse(int *a, int left, int right)
     }
 }
 
-void rotate(int *nums, int numsSize, int k)
-{
-    // �������÷�
-    // ����K����ֹkԽ�� ����numsSize
+void rotate(int *nums, int numsSize, int k) {
     k %= numsSize;
-    // 1.��������
     reverse(nums, 0, numsSize - 1);
-    // 2.ǰk������
     reverse(nums, 0, k - 1);
-    // 3.��k������
     reverse(nums, k, numsSize - 1);
 }
 
