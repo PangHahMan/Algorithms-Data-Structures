01ac02c6f01acafbba75a03311fa02ea7c49c982
PangHahMan <3278475092@qq.com>
2023-08-13 15:41:12 +0800

    简单

---
+++ 2.链表/LeetCode/CPP-Code/相交链表.cpp
@@ -0,0 +1,50 @@
+//https://leetcode.cn/problems/intersection-of-two-linked-lists/description/
+
+#include <cmath>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int countA = 0, countB = 0;
+        ListNode *curA = headA;
+        ListNode *curB = headB;
+        //计算curA的节点个数
+        while (curA) {
+            countA++;
+            curA = curA->next;
+        }
+        //计算curB的节点个数
+        while (curB) {
+            countB++;
+            curB = curB->next;
+        }
+        //计算差值
+        int gap = abs(countA - countB);
+        //让长的哪个链表先走gap步
+        ListNode *longList = headA;
+        ListNode *shortList = headB;
+        if (countA < countB) {
+            longList = headB;
+            shortList = headA;
+        }
+        //长链表先走
+        while (gap--) {
+            longList = longList->next;
+        }
+        //一起走，直到相等
+        while (longList && shortList) {
+            if (longList == shortList) {
+                return longList;
+            }
+            longList = longList->next;
+            shortList = shortList->next;
+        }
+        return nullptr;
+    }
+};
