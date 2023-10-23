8fdd0d056ecd71c55cf6f5f1333738612975ae31
PangHahMan <3278475092@qq.com>
2023-10-23 22:59:27 +0800

    1

--- LeetCode/向量/03.只出现一次的数字Ⅱ(中等).cpp
+++ LeetCode/向量/03.只出现一次的数字Ⅱ(中等).cpp
@@ -1,19 +1,13 @@
 /* 
-给你一个整数数组 nums，其中恰好有两个元素只出现一次，其余所有元素均出现两次。 找出只出现一次的那两个元素。你可以按 任意顺序 返回答案。
-你必须设计并实现线性时间复杂度的算法且仅使用常量额外空间来解决此问题。
+给你一个整数数组nums ，除某个元素仅出现一次外，其余每个元素都恰出现三次。请你找出并返回那个只出现了一次的元素。
 
 示例 1：
-输入：nums = [1,2,1,3,2,5]
-输出：[3,5]
-解释：[5, 3] 也是有效的答案。
+输入：nums = [2,2,3,2]
+输出：3
 
 示例 2：
-输入：nums = [-1,0]
-输出：[-1,0]
-
-示例 3：
-输入：nums = [0,1]
-输出：[1,0] 
+输入：nums = [0,1,0,1,0,1,99]
+输出：99
 */
 
 #include <unordered_map>
@@ -21,18 +15,62 @@
 using namespace std;
 class Solution {
 public:
-    int singleNumber(vector<int> &nums) {
+    vector<int> singleNumber(vector<int> &nums) {
+        // 创建一个无序哈希表，用于存储数字和其出现频次的关系
         unordered_map<int, int> freq;
+
+        // 遍历输入数组，统计每个数字的出现频次
         for (int num: nums) {
-            ++freq[num];
+            freq[num]++;
         }
-        int ans = 0;
+
+        vector<int> ans;
+
+        // 遍历哈希表中的每个键值对，自动拆解为数字和频次
         for (auto num: freq) {
             if (num.second == 1) {
-                ans = num.first;
+                ans.push_back(num.first);// 将出现频次为 1 的数字添加到结果向量中
+            }
+        }
+
+        return ans;// 返回存储了只出现一次的数字的向量
+    }
+};
+
+
+//方法2 异或
+class Solution {
+public:
+    vector<int> singleNumber(vector<int> &nums) {
+        //1.整体异或
+        int result = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            result ^= nums[i];
+        }
+
+        //2.分解找到第一个比特位为1
+        int flag = 1;
+        int length = sizeof(int);
+        for (int i = 0; i < length; i++) {
+            if ((flag << i) & result) {
+                flag <<= i;//找到1的位置
                 break;
             }
         }
-        return ans;
+
+        //3.分组异或
+        int num1 = 0;
+        int num2 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] & flag) {
+                num1 ^= nums[i];
+            } else {
+                num2 ^= nums[i];
+            }
+        }
+
+        return {num1, num2};
     }
 };
+
+//https://leetcode.cn/problems/single-number-ii/description/
