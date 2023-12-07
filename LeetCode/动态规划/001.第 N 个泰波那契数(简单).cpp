5e98ede3b3003c14889351e0f1162252711625eb
PangHahMan <3278475092@qq.com>
2023-12-07 17:49:13 +0800

    1

--- LeetCode/动态规划/001.第 N 个泰波那契数(简单).cpp
+++ LeetCode/动态规划/001.第 N 个泰波那契数(简单).cpp
@@ -1,5 +1,5 @@
-//https://leetcode.cn/problems/n-th-tribonacci-number/description/
-/*泰波那契序列 Tn 定义如下： 
+/* https://leetcode.cn/problems/n-th-tribonacci-number/description/
+泰波那契序列 Tn 定义如下： 
 T0 = 0, T1 = 1, T2 = 1, 且在 n >= 0 的条件下 Tn+3 = Tn + Tn+1 + Tn+2
 给你整数 n，请返回第 n 个泰波那契数 Tn 的值。
 示例 1：
@@ -20,12 +20,6 @@ using namespace std;
 class Solution {
 public:
     int tribonacci(int n) {
-        if(n == 0){
-            return 0;
-        }
-        if(n == 1 || n == 2){
-            return 1;
-        }
         //1.创建dp表
         vector<int> dp(n + 1);
         //2.初始化
