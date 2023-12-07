8e1bfd8d66c1f69330f20359e1a71a84c6b0b262
PangHahMan <3278475092@qq.com>
2023-12-07 18:42:29 +0800

    1

--- LeetCode/优选算法/双指针/02.复写零(中等).cpp
+++ LeetCode/优选算法/双指针/02.复写零(中等).cpp
@@ -13,4 +13,43 @@
 解释：调用函数后，输入的数组将被修改为：[1,2,3] 
 */
 
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    void duplicateZeros(vector<int> &arr) {
+        int cur = 0, dest = -1;
+        int n = arr.size();
+        while (cur < n) {
+            if (arr[cur]) {
+                dest++;
+            } else {
+                dest += 2;
+            }
+            if (dest >= n - 1) {
+                break;
+            }
+            cur++;
+        }
 
+        /* dest等于n表示在最后一次增加dest时，我们是因为原数组的cur位置是零而增加了2。这意味着原数组的最后一个零是无法复制的，因为它会超出数组末尾。所以，我们要做的是：
+        1.将原数组的最后一个位置设为零。
+        2.cur需要回退一个位置，因为我们不需要再复制这个无法复制的最后一个零。
+        3.dest需要减去2，一是因为最后一个零不复制，二是回退到倒数第二个应该复制的位置上。 */
+        if (dest == n) {
+            arr[n - 1] = 0;  
+            cur--;
+            dest -= 2;
+        }
+
+        while (cur >= 0) {
+            if (arr[cur]) {
+                arr[dest--] = arr[cur--];
+            } else {
+                arr[dest--] = 0;
+                arr[dest--] = 0;
+                cur--;
+            }
+        }
+    }
+};
